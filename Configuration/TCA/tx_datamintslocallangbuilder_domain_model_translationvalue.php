091a83ad2edda80a05937fbd4f82fe66835eed4a
Mark Weisgerber <m.weisgerber@datamints.com>
2023-07-18 10:46:14 +0200

    [TASK] added plain output without VHs

--- Configuration/TCA/tx_datamintslocallangbuilder_domain_model_translationvalue.php
+++ Configuration/TCA/tx_datamintslocallangbuilder_domain_model_translationvalue.php
@@ -143,7 +143,7 @@ return [
                         '0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
                     ]
                 ],
-                'default' => 0,
+                'default' => 1,
             ]
         ],
         'comment' => [
