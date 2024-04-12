1c2d4b1db99e90ddeaaf9eb034d38b985e34e341
Mark Weisgerber <m.weisgerber@datamints.com>
2024-04-12 20:21:41 +0200

    [TASK] Reduced the ProviderServer to pass the extension config value

--- Classes/Service/ProviderService.php
+++ Classes/Service/ProviderService.php
@@ -20,18 +20,6 @@ class ProviderService extends AbstractService
      */
     public function getConfiguredProvider(): string
     {
-        $settingsRef = $this->getSettings();
-        // Checking if azure is configured
-        if($settingsRef['providers']) {
-            if(\intval($settingsRef['providers']['azure']['active']) == 1 && $settingsRef['providers']['azure']['key']) {
-                return 'AzureCloud';
-            } else if(\intval($settingsRef['providers']['google']['active']) == 1 && $settingsRef['providers']['google']['key']) {
-                return 'GoogleTranslate';
-            } else if(\intval($settingsRef['providers']['deepl']['active']) == 1 && $settingsRef['providers']['deepl']['key']) {
-                return 'DeepL';
-            }
-        }
-
-        return '';
+        return $this->getExtensionConfig()['activeProvider'];
     }
 }
